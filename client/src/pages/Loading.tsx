5ffcdf5ae5083f510e51bbd78739d9e53564b9c9
Gonr <104718753+zim00@users.noreply.github.com>
2023-10-14 21:21:20 +0300

    update loading page

--- client/src/pages/Loading.tsx
+++ client/src/pages/Loading.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react'
 const LoadingPage: FC = () => {
 	return (
-		<div className='min-h-screen bg-slate-900 font-roboto text-white pb-20 text-center flex items-center'>
-			loading
+		<div className='min-h-screen bg-slate-900 font-roboto text-white pb-20 text-center flex items-center justify-center '>
+			<h1 className='text-lg'>Loading...</h1>
 		</div>
 	)
 }
--- client/src/pages/Transaction.tsx
+++ client/src/pages/Transaction.tsx
@@ -1,9 +1,35 @@
 import { FC } from 'react'
 const Transaction: FC = () => {
 	return (
-		<div>
-			<h1>Auth</h1>
-		</div>
+		<>
+			<div className='grid grid-cols-3 gap-4 mt-4 items-start'>
+				<div className='grid col-span-2'>Add transaction form</div>
+				<div className='rounded-md bg-slate-800 p-3'>
+					<div className='grid grid-cols-2 gap-3'>
+						<div className=''>
+							<p className='uppercase text-md font-bold text-center '>
+								Total Income:
+							</p>
+							<p className='bg-green-600 p-1 rounded-small text-center nt-2'>
+								1000$
+							</p>
+						</div>
+
+						<div className=''>
+							<p className='uppercase text-md font-bold text-center '>
+								Total Expense:
+							</p>
+							<p className='bg-red-600 p-1 rounded-small text-center nt-2'>
+								1000$
+							</p>
+						</div>
+					</div>
+					<p>Chart</p>
+				</div>
+			</div>
+
+			<h1 className='my-5'>Table</h1>
+		</>
 	)
 }
 export default Transaction
