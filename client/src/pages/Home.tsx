eb08a9860bc1ba8090b09820ddb29ce664c826f9
Gonr <104718753+zim00@users.noreply.github.com>
2023-11-02 14:25:43 +0300

    change home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,22 @@ import { FC } from 'react'
 const Home: FC = () => {
 	return (
 		<div>
-			<h1>Home</h1>
+			<h1 style={{ textAlign: 'center', fontSize: 30, padding: 30 }}>
+				Simple fifnance app
+			</h1>
+			<h1
+				style={{
+					fontSize: 25,
+					color: '#ff8c8c',
+					textAlign: 'center',
+					marginTop: 20,
+				}}
+			>
+				Author :{' '}
+				<a target='_blanck' href='https://github.com/gonrs'>
+					https://github.com/gonrs
+				</a>{' '}
+			</h1>
 		</div>
 	)
 }
